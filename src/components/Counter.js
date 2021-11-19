17a4138385de35fa26c218a9ce17129ffae7de01
jrraymundo <jerome_raymundo@outlook.com>
2021-11-19 20:51:25 +0800

    Subscribe to store using useSelector

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,21 @@
+import { useSelector } from 'react-redux'
 import classes from './Counter.module.css';
 
 const Counter = () => {
+  /** 
+   * When using function components,
+   * the useSelector() hook allows us to get a specific state from the redux store
+   * It also automatically handles the subscription of your component to the store,
+   * as well as unsubscribing to the store when the component unmounts
+   */
+  const counter = useSelector(state => state.counter)
+
   const toggleCounterHandler = () => {};
 
   return (
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
-      <div className={classes.value}>-- COUNTER VALUE --</div>
+      <div className={classes.value}>{counter}</div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
