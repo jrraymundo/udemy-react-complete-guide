d41e2bff4eb9e8bfe1b9168153b0a5719a6036a8
jrraymundo <jerome_raymundo@outlook.com>
2021-11-19 20:33:33 +0800

    Create provider

--- src/index.js
+++ src/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 
 import './index.css';
 import App from './App';
+import store from './store/index'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/**
+ * To make the states in redux store accessible to the whole app
+ * a Provider component from react-redux is used to wrap the whole app
+ * and pass the store as props
+ */
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
