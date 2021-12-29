42318ad272c920c8549523e30f51f6dd29cba883
jrraymundo <jerome_raymundo@outlook.com>
2021-12-29 18:44:43 +0800

    Add CSS modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,41 +1,16 @@
-import styled from 'styled-components'
+import React from 'react'
 
-import './Button.css';
+/** Using CSS Modules */
+import styles from './Button.module.css'
 
-/**
- * styled is an object that contains all kinds of HTML elements
- * 
- * styled.button will return a button component
- * and by using template literals or backticks, we can inject css to the returned button
- * 
- * When the styled button is rendered, styled-components generates and assigns a unique className
- * to the component which is good because it handles scoping of css automatically for us
- */
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  )
+}
 
-const Button = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
-
-  @media (min-width: 768px) {
-    width: auto;
-  }
-
-  &:focus {
-    outline: none;
-  }
-
-  &:hover,
-  &:active {
-    background: #ac0e77;
-    border-color: #ac0e77;
-    box-shadow: 0 0 8px rgba(0, 0, 0, 0.26);
-  }
-`
+/** Check git history for styled components version */
 
 export default Button;
