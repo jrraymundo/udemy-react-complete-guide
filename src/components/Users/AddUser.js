3e14d07e4957397c9bad173e4197dfae8f93c854
jrraymundo <jerome_raymundo@outlook.com>
2021-12-31 12:52:09 +0800

    Using refs to handle form

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,42 +1,51 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 
 import Card from '../UI/Card';
 import Button from '../UI/Button';
 import ErrorModal from '../UI/ErrorModal';
 import classes from './AddUser.module.css';
 
+/**
+ * In this example, we alternatively use useRef to handle the form instead of states
+ * 
+ * useRef returns the actual DOM node of an element which in this case
+ * allows us to see what value an input has and control the form from there
+ * 
+ * Note that in general you should only use refs for reading values and not manipulating the DOM directly
+ * React should still be used with manipulating the DOM but this is an example of what refs can do
+ */
+
 const AddUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState('');
-  const [enteredAge, setEnteredAge] = useState('');
+  const nameInputRef = useRef()
+  const ageInputRef = useRef()
+
   const [error, setError] = useState();
 
   const addUserHandler = (event) => {
     event.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    // Get value of input fields on submit using ref
+    const enteredName = nameInputRef.current.value
+    const enteredUserAge = ageInputRef.current.value
+
+    if (enteredName.trim().length === 0 || enteredUserAge.trim().length === 0) {
       setError({
         title: 'Invalid input',
         message: 'Please enter a valid name and age (non-empty values).',
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+enteredUserAge < 1) {
       setError({
         title: 'Invalid age',
         message: 'Please enter a valid age (> 0).',
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('');
-    setEnteredAge('');
-  };
+    props.onAddUser(enteredName, enteredUserAge);
 
-  const usernameChangeHandler = (event) => {
-    setEnteredUsername(event.target.value);
-  };
-
-  const ageChangeHandler = (event) => {
-    setEnteredAge(event.target.value);
+    // Reset input field to empty string
+    nameInputRef.current.value = ''
+    ageInputRef.current.value = ''
   };
 
   const errorHandler = () => {
@@ -58,15 +67,13 @@ const AddUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChangeHandler}
+            ref={nameInputRef}
           />
           <label htmlFor="age">Age (Years)</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChangeHandler}
+            ref={ageInputRef}
           />
           <Button type="submit">Add User</Button>
         </form>
