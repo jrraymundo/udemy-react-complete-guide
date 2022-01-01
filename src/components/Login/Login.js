7dd568b692fdac4bc8f948dda7357cb6e8a7f66f
jrraymundo <jerome_raymundo@outlook.com>
2022-01-01 17:59:27 +0800

    Correct initState for password and email

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -54,12 +54,12 @@ const Login = (props) => {
 
   const [emailState, dispatchEmail] = useReducer(emailReducer, {
     value: '',
-    isValid: false
+    isValid: null
   })
 
   const [passwordState, dispatchPassword] = useReducer(passwordReducer, {
     value: '',
-    isValid: false
+    isValid: null
   })
 
   /**
