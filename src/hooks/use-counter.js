16bf4aa5170c51d81a27ff450f72527293ccd338
jrraymundo <jerome_raymundo@outlook.com>
2022-01-09 16:27:51 +0800

    Add increment argument to useCounter hook

--- src/hooks/use-counter.js
+++ src/hooks/use-counter.js
@@ -1,15 +1,30 @@
 import { useState, useEffect } from 'react'
 
-export default function useCounter() {
+/**
+ * By creating custom hooks we are able to isolate some state and effect logic
+ * that we can reuse in multiple components
+ * 
+ * All custom hooks must be named starting with the word "use"
+ * And then return specific values that you want your main component to receive
+ * 
+ * Arguments may also be passed like a normal function
+ * In this case, we add an argument called "increment" with a default value of true
+ * The hook will either add or subtract the counter value depending on the argument
+ */
+export default function useCounter(increment = true) {
     const [counter, setCounter] = useState(0)
 
     useEffect(() => {
         const interval = setInterval(() => {
-        setCounter((prevCounter) => prevCounter + 1);
+            if (increment) {
+                setCounter((prevCounter) => prevCounter + 1);
+            } else {
+                setCounter((prevCounter) => prevCounter - 1);
+            }
         }, 1000)
 
         return () => clearInterval(interval)
-    }, [])
+    }, [increment])
 
     return counter
 }
