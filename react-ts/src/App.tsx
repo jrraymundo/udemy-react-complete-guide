07997e055c694cb59be64fa3e81406473dc60632
jrraymundo <jerome_raymundo@outlook.com>
2022-01-11 15:30:38 +0800

    Add useState with ts

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,15 +1,15 @@
+import { useState } from 'react';
+
 import NewTodo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo'
 
 function App() {
-  const todos = [
-    new Todo('Learn React'),
-    new Todo('Learn TypeScript')
-  ]
+  const [todos, setTodos] = useState<Todo[]>([])
 
-  const addTodoHandler = (text: string): void => {
-    return
+  const addTodoHandler = (todoText: string): void => {
+    const newTodo = new Todo(todoText)
+    setTodos(prevTodos => [...prevTodos, newTodo])
   }
 
   return (
