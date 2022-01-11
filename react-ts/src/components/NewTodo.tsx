d912af5fc007bda7cfc9b44bce105fb52b3ff76e
jrraymundo <jerome_raymundo@outlook.com>
2022-01-11 14:48:34 +0800

    Add refs for react TS

--- react-ts/src/components/NewTodo.tsx
+++ react-ts/src/components/NewTodo.tsx
@@ -1,5 +1,7 @@
+import { useRef } from "react"
 
 /**
+ * EVENTS
  * When writing type definitions for events, use React's pre-defined types.
  * It takes care of the type definitions for built-in properties/methods of events 
  * 
@@ -9,16 +11,41 @@
  *  etc.
  */
 
+/**
+ * REFS
+ * When writing type definitions for refs you need to specify which HTML element it will be assigned to
+ * by using Generics, and the HTML element should be based on the what interface it is from
+ * which can be checked on mdn web docs if ever.
+ * 
+ * Initial value for refs are required.
+ * 
+ * If you look closer at the line below, it's using a punctuation mark "!"
+ * TypeScript knows that refs can be undefined at some point, and in order to tell
+ * it that we are sure that the ref will never be undefined at this point we add "!"
+ * 
+ * const enteredText = todoTextInputRef.current!.value
+ * 
+ * However, if we are unsure or know that it can be undefined then we use "?"
+ */
+
 const NewTodo = () => {
+    const todoTextInputRef = useRef<HTMLInputElement>(null)
+
     const submitHandler = (event: React.FormEvent) => {
         event.preventDefault()
-        console.log(event)
+
+        const enteredText = todoTextInputRef.current!.value
+
+        if (enteredText.trim().length === 0) {
+            // throw an error
+            return
+        }
     }
 
     return (
         <form onSubmit={submitHandler}>
             <label htmlFor='text'>Todo Text</label>
-            <input type='text' id='text' />
+            <input type='text' id='text' ref={todoTextInputRef} />
             <button>Add Todo</button>
         </form>
     )
