0c854a421c2a39a561fb0517847dff1df7b3fa8e
jrraymundo <jerome_raymundo@outlook.com>
2022-01-12 14:07:00 +0800

    Add useRouter

--- nextjs-app/pages/news/[newsId].js
+++ nextjs-app/pages/news/[newsId].js
@@ -1,8 +1,17 @@
+import React from 'react'
+import { useRouter } from 'next/router'
+
 /**
  * Dynamic Page
  * domain-name.com/news/[newsId]
  */
 
 export default function DetailPage() {
+    const router = useRouter()
+    
+    // We are able to access newsId based on what we named this [file]
+    const newsId = router.query.newsId 
+    console.log(newsId)
+
     return <h1>The Detail Page</h1>
 }
