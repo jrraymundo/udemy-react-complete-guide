cdf0da1396f92f1bbe950efc6f70cb67d9092a3b
jrraymundo <jerome_raymundo@outlook.com>
2022-01-12 14:17:13 +0800

    Add Link from nextjs

--- nextjs-app/pages/news/index.js
+++ nextjs-app/pages/news/index.js
@@ -1,5 +1,24 @@
 // domain-name.com/news
 
+import Link from 'next/link'
+import { Fragment } from 'react';
+
 export default function NewsPage() {
-    return <h1>The News Page</h1>
+    return (
+        <Fragment>
+            <h1>The News Page</h1>
+            <ul>
+                <li>
+                    <Link href='/news/yoooo'>
+                        Yo
+                    </Link>
+                </li>
+                <li>
+                    <Link href='/news/something-else'>
+                        Something Else
+                    </Link>
+                </li>
+            </ul>
+        </Fragment>
+    )
 }
