3c7225ef4bdc992e2b660fcb91eaaf806237e836
jrraymundo <jerome_raymundo@outlook.com>
2022-01-22 17:29:37 +0800

    Add getServerSideProps

--- nextjs-app-b/pages/index.js
+++ nextjs-app-b/pages/index.js
@@ -27,11 +27,34 @@ const DUMMY_MEETUPS = [
     It's required that you return an object with a "props" property,
     and that's where you pass the data that you want your component to receive.
 */
-export async function getStaticProps() {
+// export async function getStaticProps() {
+//     // Fetch data here from an api
+//     return {
+//         props: {
+//             test: "testing",
+//             meetups: DUMMY_MEETUPS
+//         }
+//     }
+// }
+
+/*
+    Similar to `getStaticProps`. It’s also a reserved function that NextJS runs in the server side. 
+    It runs similarly, but this function gets executed on the server side on runtime (every request) 
+    which makes it a better choice if you need some data to be updated or checked really frequently.
+
+    Similar to `ExpressJS`, it takes an argument called `context` 
+    that you can use to access server-side information like headers, cookies etc.
+
+    If you don’t need any of these features, 
+    then `getStaticProps` is a better choice because it’s faster.
+*/
+export async function getServerSideProps(context) {
+    const {req, res} = context
+
     // Fetch data here from an api
+
     return {
         props: {
-            test: "testing",
             meetups: DUMMY_MEETUPS
         }
     }
