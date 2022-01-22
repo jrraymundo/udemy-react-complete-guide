363a2b1b66c05807f7a1f6503d6eebf64e4bee8f
jrraymundo <jerome_raymundo@outlook.com>
2022-01-22 14:19:39 +0800

    Add global navbar and layout

--- nextjs-app-b/pages/_app.js
+++ nextjs-app-b/pages/_app.js
@@ -1,7 +1,19 @@
+import Layout from '../components/layout/Layout'
 import '../styles/globals.css'
 
+/*
+  This _app.js file is the root component of a next.js project.
+  Component is the component that holds the page or content that should be displayed on a given route
+  When we want global components like navbar that should be displayed on all pages, 
+  then this is where they should be rendered.
+*/
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  )
 }
 
 export default MyApp
