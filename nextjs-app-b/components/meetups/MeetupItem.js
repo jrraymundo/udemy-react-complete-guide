ed6b65a8394ae26136a29f67659f3e9ee7cb6c1f
jrraymundo <jerome_raymundo@outlook.com>
2022-01-22 14:31:03 +0800

    Add programmatic nav handling with useRouter

--- nextjs-app-b/components/meetups/MeetupItem.js
+++ nextjs-app-b/components/meetups/MeetupItem.js
@@ -1,7 +1,15 @@
+import { useRouter } from 'next/router'
+
 import Card from '../ui/Card';
 import classes from './MeetupItem.module.css';
 
 function MeetupItem(props) {
+  const router = useRouter()
+
+  function showDetailsHandler() {
+    router.push('/' + props.id)
+  }
+
   return (
     <li className={classes.item}>
       <Card>
@@ -13,7 +21,11 @@ function MeetupItem(props) {
           <address>{props.address}</address>
         </div>
         <div className={classes.actions}>
-          <button>Show Details</button>
+          {/* 
+            This button is created just to show how to navigate to another page programmatically,
+            but note that using a <Link/> is actually the best option here
+           */}
+          <button onClick={showDetailsHandler}>Show Details</button>
         </div>
       </Card>
     </li>
