2ac950aa7bd10b8cdbf77d4681aa188e3011caa3
jrraymundo <jerome_raymundo@outlook.com>
2022-01-24 17:33:01 +0800

    Add api route and mongodb

--- nextjs-app-b/pages/api/new-meetup.js
+++ nextjs-app-b/pages/api/new-meetup.js
@@ -1,8 +1,22 @@
-export default function handler(req, res) {
+import { MongoClient } from 'mongodb'
+
+export default async function handler(req, res) {
     if (req.method == "GET") {
         res.json({ message: "This is a GET request response" })
     }
     if (req.method === "POST") {
-        res.json({ message: "This is a POST request response" })
+        const data = req.body
+
+        const client = await MongoClient.connect(process.env.DB_HOST)
+        const db = client.db()
+
+        const meetupsCollection = db.collection('meetups')
+
+        const result = await meetupsCollection.insertOne(data)
+        console.log(result)
+
+        client.close()
+
+        res.status(201).json({ message: 'Meetup inserted' })
     }
 }
